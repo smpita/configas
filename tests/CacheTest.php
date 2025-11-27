29a8b01c4ee3f612397b7b23c6af8f286016828f
Sean Pearce <14220530+smpita@users.noreply.github.com>
2025-11-26 23:17:34 -0800

    Add missing cache tests

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -14,8 +14,8 @@ it('can cache arrays', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::array($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache bools', function () {
@@ -28,8 +28,8 @@ it('can cache bools', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::bool($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache classes', function () {
@@ -42,8 +42,8 @@ it('can cache classes', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::class(ClassStub::class, $key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache floats', function () {
@@ -56,8 +56,8 @@ it('can cache floats', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::float($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache ints', function () {
@@ -70,8 +70,8 @@ it('can cache ints', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::int($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache nullable arrays', function () {
@@ -84,8 +84,8 @@ it('can cache nullable arrays', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::nullableArray($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache nullable bools', function () {
@@ -98,8 +98,8 @@ it('can cache nullable bools', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::nullableBool($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache nullable classes', function () {
@@ -112,8 +112,8 @@ it('can cache nullable classes', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::nullableClass(ClassStub::class, $key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache nullable floats', function () {
@@ -126,8 +126,8 @@ it('can cache nullable floats', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::nullableFloat($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache nullable ints', function () {
@@ -140,8 +140,8 @@ it('can cache nullable ints', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::nullableInt($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache nullable strings', function () {
@@ -154,8 +154,8 @@ it('can cache nullable strings', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::string($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can cache strings', function () {
@@ -168,8 +168,8 @@ it('can cache strings', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::nullableString($key);
-    expect($value)->toEqual($cached);
-    expect($value)->not->toEqual($new);
+    expect($value)->toBe($cached);
+    expect($value)->not->toBe($new);
 });
 
 it('can read fresh arrays', function () {
@@ -182,8 +182,8 @@ it('can read fresh arrays', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshArray($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh bools', function () {
@@ -196,8 +196,8 @@ it('can read fresh bools', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshBool($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh classes', function () {
@@ -210,8 +210,8 @@ it('can read fresh classes', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshClass(ClassStub::class, $key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh floats', function () {
@@ -224,8 +224,8 @@ it('can read fresh floats', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshFloat($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh ints', function () {
@@ -238,8 +238,8 @@ it('can read fresh ints', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshInt($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh nullable arrays', function () {
@@ -252,8 +252,8 @@ it('can read fresh nullable arrays', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshNullableArray($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh nullable bools', function () {
@@ -266,8 +266,8 @@ it('can read fresh nullable bools', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshNullableBool($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh nullable classes', function () {
@@ -280,8 +280,8 @@ it('can read fresh nullable classes', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshNullableClass(ClassStub::class, $key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh nullable floats', function () {
@@ -294,8 +294,8 @@ it('can read fresh nullable floats', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshNullableFloat($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh nullable ints', function () {
@@ -308,8 +308,8 @@ it('can read fresh nullable ints', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshNullableInt($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh nullable strings', function () {
@@ -322,8 +322,8 @@ it('can read fresh nullable strings', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshString($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('can read fresh strings', function () {
@@ -336,56 +336,56 @@ it('can read fresh strings', function () {
     Config::set($key, $new);
 
     $value = ConfigAs::freshNullableString($key);
-    expect($value)->toEqual($new);
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe($new);
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default arrays', function () {
     $cached = [fake()->word()];
     $value = ConfigAs::array('testing.array', $cached);
 
-    expect($value)->toEqual(ConfigAs::freshArray('testing.array'));
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe(ConfigAs::freshArray('testing.array'));
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default bools', function () {
     $cached = false;
     $value = ConfigAs::bool('testing.bool', $cached);
 
-    expect($value)->toEqual(ConfigAs::freshBool('testing.bool'));
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe(ConfigAs::freshBool('testing.bool'));
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default classes', function () {
     $cached = new CacheClassStub();
     $value = ConfigAs::class(ClassStub::class, 'testing.class', $cached);
 
-    expect($value)->toEqual(ConfigAs::freshClass(ClassStub::class, 'testing.class'));
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe(ConfigAs::freshClass(ClassStub::class, 'testing.class'));
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default floats', function () {
     $cached = fake()->randomFloat(2, 0.01, PHP_FLOAT_MAX);
     $value = ConfigAs::float('testing.float', $cached);
 
-    expect($value)->toEqual(ConfigAs::freshFloat('testing.float'));
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe(ConfigAs::freshFloat('testing.float'));
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default ints', function () {
     $cached = fake()->numberBetween(1, PHP_INT_MAX);
     $value = ConfigAs::int('testing.int', $cached);
 
-    expect($value)->toEqual(ConfigAs::freshInt('testing.int'));
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe(ConfigAs::freshInt('testing.int'));
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default strings', function () {
     $cached = fake()->word();
     $value = ConfigAs::string('testing.string', $cached);
 
-    expect($value)->toEqual(ConfigAs::freshString('testing.string'));
-    expect($value)->not->toEqual($cached);
+    expect($value)->toBe(ConfigAs::freshString('testing.string'));
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default nullable arrays', function () {
@@ -393,7 +393,7 @@ it('does not cache default nullable arrays', function () {
     $value = ConfigAs::nullableArray('testing.nullable');
 
     expect($value)->toBeNull();
-    expect($value)->not->toEqual($cached);
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default nullable bools', function () {
@@ -401,7 +401,7 @@ it('does not cache default nullable bools', function () {
     $value = ConfigAs::nullableBool('testing.nullable');
 
     expect($value)->toBeNull();
-    expect($value)->not->toEqual($cached);
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default nullable classes', function () {
@@ -409,7 +409,7 @@ it('does not cache default nullable classes', function () {
     $value = ConfigAs::nullableClass(CacheClassStub::class, 'testing.nullable');
 
     expect($value)->toBeNull();
-    expect($value)->not->toEqual($cached);
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default nullable floats', function () {
@@ -417,7 +417,7 @@ it('does not cache default nullable floats', function () {
     $value = ConfigAs::nullableFloat('testing.nullable');
 
     expect($value)->toBeNull();
-    expect($value)->not->toEqual($cached);
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default nullable ints', function () {
@@ -425,7 +425,7 @@ it('does not cache default nullable ints', function () {
     $value = ConfigAs::nullableInt('testing.nullable');
 
     expect($value)->toBeNull();
-    expect($value)->not->toEqual($cached);
+    expect($value)->not->toBe($cached);
 });
 
 it('does not cache default nullable strings', function () {
@@ -433,7 +433,175 @@ it('does not cache default nullable strings', function () {
     $value = ConfigAs::nullableString('testing.nullable');
 
     expect($value)->toBeNull();
-    expect($value)->not->toEqual($cached);
+    expect($value)->not->toBe($cached);
+});
+
+it('can forget arrays', function () {
+    $key = 'testing.array';
+    $value = ['foo'];
+    $new = [];
+
+    expect(ConfigAs::array($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::array($key))->toBe($value);
+
+    ConfigAs::forgetArray($key);
+    expect(ConfigAs::array($key))->toBe($new);
+    expect(ConfigAs::array($key))->not->toBe($value);
+});
+
+it('can forget bools', function () {
+    $key = 'testing.bool';
+    $value = true;
+    $new = false;
+
+    expect(ConfigAs::bool($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::bool($key))->toBe($value);
+
+    ConfigAs::forgetBool($key);
+    expect(ConfigAs::bool($key))->toBe($new);
+    expect(ConfigAs::bool($key))->not->toBe($value);
+});
+
+it('can forget classes', function () {
+    $key = 'testing.class';
+    $value = Config::get($key); // new ClassStub()
+    $new = new CacheClassStub();
+
+    expect(ConfigAs::class(ClassStub::class, $key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::class(ClassStub::class, $key))->toBe($value);
+
+    ConfigAs::forgetClass($key);
+    expect(ConfigAs::class(CacheClassStub::class, $key))->toBe($new);
+    expect(ConfigAs::class(CacheClassStub::class, $key))->not->toBe($value);
+});
+
+it('can forget floats', function () {
+    $key = 'testing.float';
+    $value = 123.456;
+    $new = 876.5309;
+
+    expect(ConfigAs::float($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::float($key))->toBe($value);
+
+    ConfigAs::forgetFloat($key);
+    expect(ConfigAs::float($key))->toBe($new);
+    expect(ConfigAs::float($key))->not->toBe($value);
+});
+
+it('can forget ints', function () {
+    $key = 'testing.int';
+    $value = 123;
+    $new = 54321;
+
+    expect(ConfigAs::int($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::int($key))->toBe($value);
+
+    ConfigAs::forgetInt($key);
+    expect(ConfigAs::int($key))->toBe($new);
+    expect(ConfigAs::int($key))->not->toBe($value);
+});
+
+it('can forget strings', function () {
+    $key = 'testing.string';
+    $value = 'foo';
+    $new = 'bar';
+
+    expect(ConfigAs::string($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::string($key))->toBe($value);
+
+    ConfigAs::forgetString($key);
+    expect(ConfigAs::string($key))->toBe($new);
+    expect(ConfigAs::string($key))->not->toBe($value);
+});
+
+it('can forget nullable arrays', function () {
+    $key = 'testing.nullable.array';
+    $value = null;
+    $new = [];
+
+    expect(ConfigAs::nullableArray($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::nullableArray($key))->toBe($value);
+
+    ConfigAs::forgetNullableArray($key);
+    expect(ConfigAs::nullableArray($key))->toBe($new);
+    expect(ConfigAs::nullableArray($key))->not->toBe($value);
+});
+
+it('can forget nullable bools', function () {
+    $key = 'testing.nullable.bool';
+    $value = null;
+    $new = false;
+
+    expect(ConfigAs::nullableBool($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::nullableBool($key))->toBe($value);
+
+    ConfigAs::forgetNullableBool($key);
+    expect(ConfigAs::nullableBool($key))->toBe($new);
+    expect(ConfigAs::nullableBool($key))->not->toBe($value);
+});
+
+it('can forget nullable classes', function () {
+    $key = 'testing.nullable.class';
+    $value = null;
+    $new = new CacheClassStub();
+
+    expect(ConfigAs::nullableClass(ClassStub::class, $key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::nullableClass(ClassStub::class, $key))->toBe($value);
+
+    ConfigAs::forgetNullableClass($key);
+    expect(ConfigAs::nullableClass(CacheClassStub::class, $key))->toBe($new);
+    expect(ConfigAs::nullableClass(CacheClassStub::class, $key))->not->toBe($value);
+});
+
+it('can forget nullable floats', function () {
+    $key = 'testing.nullable.float';
+    $value = null;
+    $new = 876.5309;
+
+    expect(ConfigAs::nullableFloat($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::nullableFloat($key))->toBe($value);
+
+    ConfigAs::forgetNullableFloat($key);
+    expect(ConfigAs::nullableFloat($key))->toBe($new);
+    expect(ConfigAs::nullableFloat($key))->not->toBe($value);
+});
+
+it('can forget nullable ints', function () {
+    $key = 'testing.nullable.int';
+    $value = null;
+    $new = 54321;
+
+    expect(ConfigAs::nullableInt($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::nullableInt($key))->toBe($value);
+
+    ConfigAs::forgetNullableInt($key);
+    expect(ConfigAs::nullableInt($key))->toBe($new);
+    expect(ConfigAs::nullableInt($key))->not->toBe($value);
+});
+
+it('can forget nullable strings', function () {
+    $key = 'testing.nullable.string';
+    $value = null;
+    $new = 'bar';
+
+    expect(ConfigAs::nullableString($key))->toBe($value);
+    Config::set($key, $new);
+    expect(ConfigAs::nullableString($key))->toBe($value);
+
+    ConfigAs::forgetNullableString($key);
+    expect(ConfigAs::nullableString($key))->toBe($new);
+    expect(ConfigAs::nullableString($key))->not->toBe($value);
 });
 
 class CacheClassStub extends ClassStub
