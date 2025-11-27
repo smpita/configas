2c17ef7fbbb020a44520273c3a97420b9b815c8e
Sean Pearce <14220530+smpita@users.noreply.github.com>
2025-11-27 00:10:51 -0800

    Test default handling

--- tests/ConfigAsTest.php
+++ tests/ConfigAsTest.php
@@ -30,6 +30,11 @@ it('can handle ints', function () {
     expect($value)->toBeInt();
 });
 
+it('can handle strings', function () {
+    $value = staticStringTest(ConfigAs::string('testing.float'));
+    expect($value)->toBeString();
+});
+
 it('can handle nullable arrays', function () {
     $value = staticNullableArrayTest(ConfigAs::nullableArray('testing.nullable'));
     expect($value)->toBeNull();
@@ -60,9 +65,100 @@ it('can handle nullable strings', function () {
     expect($value)->toBeNull();
 });
 
-it('can handle strings', function () {
-    $value = staticStringTest(ConfigAs::string('testing.float'));
-    expect($value)->toBeString();
+it('can handle array default', function () {
+    $key = 'testing.default.array';
+    $default = [];
+
+    expect(ConfigAs::array($key, $default))->toBe($default);
+    expect(ConfigAs::array($key)); // throws exception without default, does not cache default
+})->throws(ConfigAsResolutionException::class);
+
+it('can handle bool default', function () {
+    $key = 'testing.default.bool';
+    $default = false;
+
+    expect(ConfigAs::bool($key, $default))->toBe($default);
+    expect(ConfigAs::bool($key)); // throws exception without default, does not cache default
+})->throws(ConfigAsResolutionException::class)->group('testing');
+
+it('can handle class default', function () {
+    $key = 'testing.default.class';
+    $default = new StdClass();
+
+    expect(ConfigAs::class(StdClass::class, $key, $default))->toBe($default);
+    expect(ConfigAs::class(StdClass::class, $key)); // throws exception without default, does not cache default
+})->throws(ConfigAsResolutionException::class);
+
+it('can handle float default', function () {
+    $key = 'testing.default.float';
+    $default = 123.456;
+
+    expect(ConfigAs::float($key, $default))->toBe($default);
+    expect(ConfigAs::float($key)); // throws exception without default, does not cache default
+})->throws(ConfigAsResolutionException::class);
+
+it('can handle int default', function () {
+    $key = 'testing.default.int';
+    $default = 123;
+
+    expect(ConfigAs::int($key, $default))->toBe($default);
+    expect(ConfigAs::int($key)); // throws exception without default, does not cache default
+})->throws(ConfigAsResolutionException::class);
+
+it('can handle string default', function () {
+    $key = 'testing.default.string';
+    $default = 'testing';
+
+    expect(ConfigAs::string($key, $default))->toBe($default);
+    expect(ConfigAs::string($key)); // throws exception without default, does not cache default
+})->throws(ConfigAsResolutionException::class);
+
+it('can handle nullable array default', function () {
+    $key = 'testing.default.nullable.array';
+    $default = [];
+
+    expect(ConfigAs::nullableArray($key, $default))->toBe($default);
+    expect(ConfigAs::nullableArray($key))->toBeNull(); // does not cache default
+});
+
+it('can handle nullable bool default', function () {
+    $key = 'testing.default.nullable.bool';
+    $default = false;
+
+    expect(ConfigAs::nullableBool($key, $default))->toBe($default);
+    expect(ConfigAs::nullableBool($key))->toBeNull(); // does not cache default
+});
+
+it('can handle nullable class default', function () {
+    $key = 'testing.default.nullable.class';
+    $default = new StdClass();
+
+    expect(ConfigAs::nullableClass(StdClass::class, $key, $default))->toBe($default);
+    expect(ConfigAs::nullableClass(StdClass::class, $key))->toBeNull(); // does not cache default
+});
+
+it('can handle nullable float default', function () {
+    $key = 'testing.default.nullable.float';
+    $default = 123.456;
+
+    expect(ConfigAs::nullableFloat($key, $default))->toBe($default);
+    expect(ConfigAs::nullableFloat($key))->toBeNull(); // does not cache default
+});
+
+it('can handle nullable int default', function () {
+    $key = 'testing.default.nullable.int';
+    $default = 123;
+
+    expect(ConfigAs::nullableInt($key, $default))->toBe($default);
+    expect(ConfigAs::nullableInt($key))->toBeNull(); // does not cache default
+});
+
+it('can handle nullable string default', function () {
+    $key = 'testing.default.nullable.string';
+    $default = 'testing';
+
+    expect(ConfigAs::nullableString($key, $default))->toBe($default);
+    expect(ConfigAs::nullableString($key))->toBeNull(); // does not cache default
 });
 
 it('throws exception when not array', function () {
