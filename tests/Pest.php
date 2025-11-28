a54a94b9b274b56c553bc000417ed4c4bac8806b
Sean Pearce <14220530+smpita@users.noreply.github.com>
2025-11-28 11:51:29 -0800

    Centralize common helper methods to

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,66 @@
 <?php
 
 use Smpita\ConfigAs\Tests\TestCase;
+use Smpita\ConfigAs\Tests\Stubs\ClassStub;
 
 uses(TestCase::class)->in(__DIR__);
+
+function staticArrayTest(array $value): array
+{
+    return $value;
+}
+
+function staticBoolTest(bool $value): bool
+{
+    return $value;
+}
+
+function staticClassTest(ClassStub $value): ClassStub
+{
+    return $value;
+}
+
+function staticFloatTest(float $value): float
+{
+    return $value;
+}
+
+function staticIntTest(int $value): int
+{
+    return $value;
+}
+
+function staticNullableArrayTest(?array $value): ?array
+{
+    return $value;
+}
+
+function staticNullableBoolTest(?bool $value): ?bool
+{
+    return $value;
+}
+
+function staticNullableClassTest(?ClassStub $value): ?ClassStub
+{
+    return $value;
+}
+
+function staticNullableFloatTest(?float $value): ?float
+{
+    return $value;
+}
+
+function staticNullableIntTest(?int $value): ?int
+{
+    return $value;
+}
+
+function staticNullableStringTest(?string $value): ?string
+{
+    return $value;
+}
+
+function staticStringTest(string $value): string
+{
+    return $value;
+}
